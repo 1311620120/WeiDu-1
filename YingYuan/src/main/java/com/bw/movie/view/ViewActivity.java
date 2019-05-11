0bbff4218d8ff2ef2317665fc05101b9cf5ba5b5
unknown <1311620120@qq.com>
2019-05-11 16:35:50 +0800

    bai

--- YingYuan/src/main/java/com/bw/movie/view/ViewActivity.java
+++ YingYuan/src/main/java/com/bw/movie/view/ViewActivity.java
@@ -3,8 +3,12 @@ package com.bw.movie.view;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.bw.movie.fragment.CinemaFragment;
+import com.bw.movie.fragment.FilmFragment;
+import com.bw.movie.fragment.MyFragment;
 import com.bw.movie.inter.MyInterface;
 import com.bw.movie.presenter.MyPresenter;
+import com.hjm.bottomtabbar.BottomTabBar;
 
 import java.util.HashMap;
 
@@ -14,8 +18,16 @@ public class ViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
-        MyInterface.PresenterInter presenterInter = new MyPresenter<>(this);
-        presenterInter.toLogi(new HashMap<String, String>());
-        presenterInter.toLogi(new HashMap<String, String>());
+        BottomTabBar buttonBar = findViewById(R.id.buttonBar);
+
+
+        buttonBar.init(getSupportFragmentManager())
+                .setTabBarBackgroundColor(0x0FFF)
+                .setFontSize(0)
+                .setImgSize(100,100)
+                .addTabItem("",R.mipmap.film_fault_xhdpi,R.mipmap.film_fault_xhdpi,FilmFragment.class)
+                .addTabItem("",R.mipmap.selected_xhdpi,R.mipmap.default_xhdpi,CinemaFragment.class)
+                .addTabItem("",R.mipmap.icon_my_selected_xhdpi,R.mipmap.icon_my_default_xhdpi,MyFragment.class);
+
     }
 }
